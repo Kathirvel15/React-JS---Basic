656d248980f0fc5a06f178d1c5734b5b73338159
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-04-27 08:12:06 +0530

    reacr-JS

--- src/Component/helloWorld.js
+++ src/Component/helloWorld.js
@@ -1,8 +1,23 @@
 import React from "react";
 function hello()
 {
+    const ff=()=>
+    {
+        document.querySelector(".paragraph").style.display="block";
+        console.log("ss");
+        console.log("Career");
+    }
     return(
-    <div className="kk">HelloWorldddd</div>
+        <div class="header-helloworld">
+        <div class="btn">
+    <button onClick={ff}>
+        click
+    </button></div>
+    <div class="paragraph"><strong>Address:</strong> 2nd Floor, Mirage Point, 34, 80 Feet Rd, 1st Block Koramangala, Koramangala, Bengaluru, Karnataka 560034<br></br>
+<strong>Hours:</strong> 
+Closed ⋅ Opens 11AM<br></br>
+<strong>Phone: </strong>063660 04441</div>
+    </div>
     );
 }
 export default hello;
